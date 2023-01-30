6e6ac03130a4f9215edbc6959d2cdc92cc3c1b54
Ravish <oceancusp@gmail.com>
2023-01-30 14:27:31 +0530

    poer modified

--- app.js
+++ app.js
@@ -38,4 +38,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+var port = process.env.PORT || 8000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
+
 module.exports = app;
